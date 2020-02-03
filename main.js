e257703c369831e5613f7c9378b2fd7689d974c7
Salman Alsuwailem <salman6111@gmail.com>
2020-02-03 16:31:12 +0300

    determine the winner and add massages for X and O turn

--- main.js
+++ main.js
@@ -1,50 +1,107 @@
-
-let board= [];
-let player1= "X";
-let player2= "O";
-let playerturn= "X";
+let playerTurn = "X";
+count= 0;
 
 
 
-const winningLogic= [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2],
+
+const winningLogic = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
 ];
 
-const eachCell= document.querySelectorAll(".cell");
-function startPlaying(){
- 
-   
+function winner() {
+
+    //X
+    if (eachCell[0].innerText === "X" && eachCell[1].innerText === "X" && eachCell[2].innerText === "X") {
+        message.innerText = ("X wins");
+    } else if (eachCell[3].innerText === "X" && eachCell[4].innerText === "X" && eachCell[5].innerText === "X") {
+        message.innerText = ("X wins");
+    } else if (eachCell[6].innerText === "X" && eachCell[7].innerText === "X" && eachCell[8].innerText === "X") {
+        message.innerText = ("X wins");
+    } else if (eachCell[0].innerText === "X" && eachCell[3].innerText === "X" && eachCell[6].innerText === "X") {
+        message.innerText = ("X wins");
+    }
+    if (eachCell[1].innerText === "X" && eachCell[4].innerText === "X" && eachCell[7].innerText === "X") {
+        message.innerText = ("X wins");
+    } else if (eachCell[2].innerText === "X" && eachCell[5].innerText === "X" && eachCell[8].innerText === "X") {
+        message.innerText = ("X wins");
+    } else if (eachCell[0].innerText === "X" && eachCell[4].innerText === "X" && eachCell[8].innerText === "X") {
+        message.innerText = ("X wins");
+    } else if (eachCell[6].innerText === "X" && eachCell[4].innerText === "X" && eachCell[2].innerText === "X") {
+        message.innerText = ("X wins");
+    }
+
+    //O
+    if (eachCell[0].innerText === "O" && eachCell[1].innerText === "O" && eachCell[2].innerText === "O") {
+        message.innerText = ("O wins");
+    } else if (eachCell[3].innerText === "O" && eachCell[4].innerText === "O" && eachCell[5].innerText === "O") {
+        message.innerText = ("O wins");
+    } else if (eachCell[6].innerText === "O" && eachCell[7].innerText === "O" && eachCell[8].innerText === "O") {
+        message.innerText = ("O wins");
+    } else if (eachCell[0].innerText === "O" && eachCell[3].innerText === "O" && eachCell[6].innerText === "O") {
+        message.innerText = ("O wins");
+    }
+    if (eachCell[1].innerText === "O" && eachCell[4].innerText === "O" && eachCell[7].innerText === "O") {
+        message.innerText = ("O wins");
+    } else if (eachCell[2].innerText === "O" && eachCell[5].innerText === "O" && eachCell[8].innerText === "O") {
+        message.innerText = ("O wins");
+    } else if (eachCell[0].innerText === "O" && eachCell[4].innerText === "O" && eachCell[8].innerText === "O") {
+        message.innerText = ("O wins");
+    } else if (eachCell[6].innerText === "O" && eachCell[4].innerText === "O" && eachCell[2].innerText === "O") {
+        message.innerText = ("O wins");
+    }
+}
+    //else {
+     //   message.innerText = ("It's a tie");
+ /////}
+
+
+const message = document.getElementById("message")
+"Choose Your Move", "Type X or O"
+
+const eachCell = document.querySelectorAll(".cell");
+
+function startPlaying() {
+
     console.log("clicked");
-       this.innerText = playerturn;
-       switchPlayer();
+    this.innerText = playerTurn;
+    switchPlayer();
+    winner();
+
     this.removeEventListener("click", startPlaying);
 }
+   
 
-for (let i = 0; i < eachCell.length; i++){
-eachCell[i].addEventListener("click",startPlaying);
+for (let i = 0; i < eachCell.length; i++) {
+    eachCell[i].addEventListener("click", startPlaying);
 }
+
 
 function switchPlayer() {
-    if (playerturn ==="X") {
-    playerturn= "O";
-    }
-    else{
-        playerturn= "X"; 
+    if (playerTurn === "X") {
+        playerTurn = "O";
+        message.innerText = ("It's " + playerTurn + "'s turn");
+    } else {
+        playerTurn = "X";
+        message.innerText = ("It's " + playerTurn + "'s turn");
     }
 }
 
- 
-    //console.log("restart");
-    //this.value =" ";
-   // this.removeEventListener("click", clickRestart);
+function restartGame() {
+    console.log("restart");
+    for (let i = 0; i < eachCell.length; i++) {
+        eachCell[i].innerText = "";
+        eachCell[i].addEventListener("click", startPlaying);
+        message.innerText = 'Start playing with X!';
+        playerTurn = "X";
 
-//for (let i = 0; i < eachCell.length; i++){
-//restart.addEventListener("click",clickRestart);
-//}
+    }
+}
+
+document.querySelector("#restart").addEventListener("click", restartGame);
